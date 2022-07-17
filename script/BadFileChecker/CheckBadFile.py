1a6b2c65fae7ea8fb5440f03252627dbfb73a5b7
jalmond <jalmond@cern.ch>
2022-07-17 22:42:47 +0900

    remove old cold remanent

--- script/BadFileChecker/CheckBadFile.py
+++ script/BadFileChecker/CheckBadFile.py
@@ -13,14 +13,10 @@ def CheckHasEntries(filepath):
 
   _localfile = open(filepath, "r")
   nevent_tree1=0
-  nevent_tree2=0
   f = ROOT.TFile(filepath)
   _tree  = f.Get("recoTree/SKFlat")
-  _tree2  = f.Get("SKFlat")
   if _tree:
     nevent_tree1 = nevent_tree1 + _tree.GetEntries()
-  if _tree2:
-    nevent_tree2 = nevent_tree2 + _tree2.GetEntries()
   f.Close()
   _localfile.close()
 
