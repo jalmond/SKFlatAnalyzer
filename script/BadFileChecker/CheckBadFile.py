dfded88e9812c3c2834bf9f406b0a40a44f9f3d7
jalmond <jalmond@cern.ch>
2022-07-17 22:27:33 +0900

    Add 2016postVFP samples

--- script/BadFileChecker/CheckBadFile.py
+++ script/BadFileChecker/CheckBadFile.py
@@ -7,3 +7,25 @@ def CheckBadFile(filepath):
   IsRecovered = f.TestBit(1024)
 
   return (IsZombie or IsRecovered)
+
+def CheckHasEntries(filepath):
+  
+
+  _localfile = open(filepath, "r")
+  nevent_tree1=0
+  nevent_tree2=0
+  f = ROOT.TFile(filepath)
+  _tree  = f.Get("recoTree/SKFlat")
+  _tree2  = f.Get("SKFlat")
+  if _tree:
+    nevent_tree1 = nevent_tree1 + _tree.GetEntries()
+  if _tree2:
+    nevent_tree2 = nevent_tree2 + _tree2.GetEntries()
+  f.Close()
+  _localfile.close()
+
+
+  if nevent_tree1 == 0 :
+    return False
+  else:
+    return True
