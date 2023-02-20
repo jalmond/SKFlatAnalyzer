43798bd11f142b84c7a5e154a73fbe9137e2e085
Hooooon12 <theking461@snu.ac.kr>
2023-02-20 17:31:19 +0900

    .

--- runJobs/HNL/HNL_ControlRegionPlotter/run_hnl.sh
+++ runJobs/HNL/HNL_ControlRegionPlotter/run_hnl.sh
@@ -10,8 +10,9 @@ njobs_sig=2
 njobs_data=100
 nmax=300
 skim=' '
-declare  -a era_list=("2016postVFP" "2016preVFP" "2017" )
-declare  -a era_list=("2018")
+#declare  -a era_list=("2016postVFP" "2016preVFP" "2017" )
+#declare  -a era_list=("2018")
+declare  -a era_list=("2017")
 
 if [[ $1 == "WG" ]]; then
 
@@ -106,10 +107,10 @@ if [[ $1 == "" ]]; then
   #SKFlat.py -a $analyzer  -l $mcpath/PromptSS.txt  -n 20  --nmax ${nmax}   -e ${i}   --skim SkimTree_HNMultiLep &
   #SKFlat.py -a $analyzer  -l $mcpath/PromptOS.txt  -n ${njobs_data}  --nmax ${nmax}   -e ${i}  --skim SkimTree_DileptonBDT  &
   SKFlat.py -a $analyzer  -l $sigpath/SSWW.txt  -n $njobs_sig  --nmax ${nmax}  -e ${i}    &
-        SKFlat.py -a $analyzer  -l $sigpath/DY.txt    -n $njobs_sig  --nmax ${nmax}   -e ${i}  &
-        SKFlat.py -a $analyzer  -l $sigpath/VBF.txt   -n $njobs_sig  --nmax ${nmax}   -e ${i}  &
+#SKFlat.py -a $analyzer  -l $sigpath/DY.txt    -n $njobs_sig  --nmax ${nmax}   -e ${i}  &
+#SKFlat.py -a $analyzer  -l $sigpath/VBF.txt   -n $njobs_sig  --nmax ${nmax}   -e ${i}  &
         #SKFlat.py -a $analyzer  -l $mcpath/Conv.txt  -n 10  --nmax ${nmax}   -e ${i} --skim SkimTree_HNMultiLep  --userflags RunConv &
-        SKFlat.py -a $analyzer  -l $mcpath/CF.txt  -n $njobs  --nmax ${nmax}   -e ${i} --skim SkimTree_HNMultiLep  --userflags RunCF &
+#SKFlat.py -a $analyzer  -l $mcpath/CF.txt  -n $njobs  --nmax ${nmax}   -e ${i} --skim SkimTree_HNMultiLep  --userflags RunCF &
         #SKFlat.py -a $analyzer  -l $datapath/${i}_DiLepton.txt  -n ${njobs_data}  --nmax ${nmax}   -e ${i}  --skim SkimTree_HNMultiLep --userflags RunFake  &
 
     done
