045698dca24eb96d62bbcf91d801e4f10792f081
jalmond <jalmond@cern.ch>
2023-10-17 22:34:16 +0900

    Add Skim DY for Egamma TnP

--- SkimScripts/SkimTree_EGammaTnP/runSkimTree_EGammaTnP.sh
+++ SkimScripts/SkimTree_EGammaTnP/runSkimTree_EGammaTnP.sh
@@ -7,17 +7,17 @@ njobs_data=2000
 nmax=300
 skim=' '
 
-declare  -a era_list=("2017" )
-# "2016preVFP" "2016postVFP" "2018")
+declare  -a era_list=("2017" "2016preVFP" "2016postVFP" "2018")
 
 if [[ $1 == "" ]]; then
 
     for i in "${era_list[@]}"
     do
 
-	SKFlat.py -a $analyzer  -i DYJets_MG    -n 6000  --nmax 300           -e ${i}   --skim SkimTree_DileptonBDT &
-        SKFlat.py -a $analyzer  -i DYJets       -n 6000  --nmax 300           -e ${i}   --skim SkimTree_DileptonBDT &
-        SKFlat.py -a $analyzer  -l $datapath/DATA_${i}_SingleLepton.txt  -n 6000  --nmax 300   -e ${i}  --skim SkimTree_DileptonBDT &
+	SKFlat.py -a $analyzer  -i DYJetsToEE_MiNNLO    -n 6000  --nmax 300           -e ${i}   --skim SkimTree_DileptonBDT &
+	#SKFlat.py -a $analyzer  -i DYJets_MG    -n 6000  --nmax 300           -e ${i}   --skim SkimTree_DileptonBDT &
+        #SKFlat.py -a $analyzer  -i DYJets       -n 6000  --nmax 300           -e ${i}   --skim SkimTree_DileptonBDT &
+        #SKFlat.py -a $analyzer  -l $datapath/DATA_${i}_SingleLepton.txt  -n 6000  --nmax 300   -e ${i}  --skim SkimTree_DileptonBDT &
 
     done
 fi
