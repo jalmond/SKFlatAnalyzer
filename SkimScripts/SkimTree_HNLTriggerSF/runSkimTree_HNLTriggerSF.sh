ec22b002f50ecd1c442ecd4d3a64bf8106363d99
jalmond <jalmond@cern.ch>
2025-01-16 02:21:13 +0900

    remove returns in skim

--- SkimScripts/SkimTree_HNLTriggerSF/runSkimTree_HNLTriggerSF.sh
+++ SkimScripts/SkimTree_HNLTriggerSF/runSkimTree_HNLTriggerSF.sh
@@ -4,8 +4,8 @@ mcpath=${SKFlat_WD}/SkimScripts/SampleLists/Bkg/
 datapath=${SKFlat_WD}/SkimScripts/SampleLists/Data/
 njobs=600
 njobs_data=600
-nmax=400
-declare  -a era_list=("2018" "2017"  "2016preVFP" "2016postVFP")
+nmax=300
+declare  -a era_list=("2017")
 
 if [[ $1 == "All" ]]; then
 
@@ -13,9 +13,10 @@ if [[ $1 == "All" ]]; then
     do
         #SKFlat.py -a $analyzer  -i DYJetsToMuMu_MiNNLO     -n ${njobs_data}  --nmax ${nmax}   -e ${i} &
         #SKFlat.py -a $analyzer  -i DYJetsToEE_MiNNLO       -n ${njobs_data}  --nmax ${nmax}   -e ${i} &
-        SKFlat.py -a $analyzer  -i TTLL_powheg             -n ${njobs_data}  --nmax ${nmax}   -e ${i} &
+        #SKFlat.py -a $analyzer  -i TTLL_powheg             -n ${njobs_data}  --nmax ${nmax}   -e ${i} &
         SKFlat.py -a $analyzer  -l $datapath/DATA_${i}_SingleLepton.txt   -n ${njobs_data}  --nmax ${nmax}   -e ${i} &
-        SKFlat.py -a $analyzer  -l $datapath/DATA_EMu_${i}.txt       -n ${njobs_data}  --nmax ${nmax}   -e ${i} 
+        #SKFlat.py -a $analyzer  -l $datapath/DATA_EMu_${i}.txt       -n ${njobs_data}  --nmax ${nmax}   -e ${i} 
+
     done
 fi
 
