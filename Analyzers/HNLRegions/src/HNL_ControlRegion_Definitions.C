ed3eb597063725d8ec5b916d45eeb6ae573a979b
jalmond <jalmond@cern.ch>
2025-05-22 18:42:51 +0900

    update pt cut in SR2 CR code

--- Analyzers/HNLRegions/src/HNL_ControlRegion_Definitions.C
+++ Analyzers/HNLRegions/src/HNL_ControlRegion_Definitions.C
@@ -1560,7 +1560,7 @@ TString  HNL_RegionDefinitions::FillHighMassSR2CRPlots(HNL_LeptonCore::Channel c
 
   
   if(!CheckLeptonFlavourForChannel(channel, leps)) return "false";
-  if(leps[1]->Pt() < 20) return "false"; 
+  if(leps[1]->Pt() < 15) return "false"; 
   
   if (param.SRConfig == "SR2_Pt20") {
     if(leps[1]->Pt() < 20) return "false";
