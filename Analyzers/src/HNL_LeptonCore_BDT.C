dafa4dc00abc8218d5d3cb9231c9768a1fe15abe
Haneol Lee <haneol.lee@cern.ch>
2025-09-10 16:52:33 +0900

    Fix a bug in SR1, add BDT plots in CR3

--- Analyzers/src/HNL_LeptonCore_BDT.C
+++ Analyzers/src/HNL_LeptonCore_BDT.C
@@ -2048,7 +2048,7 @@ double HNL_LeptonCore::EvaluateEventMVA(TString mN, TString bkgType, TString ver
                          All_JetColl, JetColl,VBF_JetColl,B_JetColl,
                          ev,METv,param);
 
-  if(isVarPlots && version=="V4" && mN=="100" && bkgType=="Incl"){
+  if(isVarPlots && version=="V3" && mN=="100" && bkgType=="Incl"){
 
     FillHist("HNL_HighMassSR3_TwoLepton_CR/"+param.Name+"/BDT/Nvbfj", ev_bdt_Nvbfj, weight, 10, 0., 10.);
     FillHist("HNL_HighMassSR3_TwoLepton_CR/"+param.Name+"/BDT/Nb", ev_bdt_Nb, weight, 10, 0., 10.);
