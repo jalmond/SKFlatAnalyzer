46e127c487d592b58f1e87232478e0db84a71995
Haneol Lee <haneol.lee@cern.ch>
2026-01-17 06:05:03 +0900

    minor changes

--- Analyzers/src/HNL_LeptonCore_Plotter.C
+++ Analyzers/src/HNL_LeptonCore_Plotter.C
@@ -811,14 +811,17 @@ void HNL_LeptonCore::Fill_Plots(AnalyzerParameter& param, TString  region,  TStr
   if(!HasFlag("Plots")) return;
   
   double HT = GetHT(jets, fatjets);
-  FillHist( plot_dir+ region+ "/SKEvent/Ev_HT", HT  , w, 200, 0.0, 2000.0,"H_{T} GeV");
+  FillHist( plot_dir+ region+ "/SKEvent/Ev_HT", HT, w, 3000, 0.0, 3000.0, "H_{T} GeV");
   
   FillHist( plot_dir+ region+ "/SKEvent/nPV",  nvtx , w, 120, 0.0, 120.);
   FillHist( plot_dir+ region+ "/SKEvent/nPileUp",  nPileUp, w, 120, 0.0, 120.);
 
   double ST = GetST(leps, jets, fatjets, met);
+  double XT = ST - met.Pt();
   
-  FillHist( plot_dir+ region+ "/SKEvent/Ev_ST", ST  , w, 250, 0.0, 5000.0,"ST GeV");
+  FillHist( plot_dir+ region+ "/SKEvent/Ev_ST", ST, w, 5000, 0.0, 5000.0, "S_{T} GeV");
+  FillHist( plot_dir+ region+ "/SKEvent/Ev_XT", XT, w, 5000, 0.0, 5000.0, "X_{T} GeV");
+  FillHist( plot_dir+ region+ "/SKEvent/XT_PT1", XT/leps[0]->Pt(), w, 100, 0.0, 10.0, "XT/PT(1)");
 
   Particle METv           = GetMiniAODvMET("T1xyCorr");
   Particle PuppiMETvULPhiCorr = GetMiniAODvMET("PuppiT1xyULCorr");
