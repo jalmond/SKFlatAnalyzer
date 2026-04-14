adfce02bf3dc4a57045ae1c75f7cdaf67085c817
jalmond <jalmond@cern.ch>
2026-04-14 21:12:46 +0900

    add config

---
+++ data_validation/analysis_validation/limit_binning_optimisation/SR1/modular_framework/config/config.py
@@ -0,0 +1,37 @@
+MASSES = [
+	"400","450","500",
+        "600","700","800","900",
+	"1000","1500","2000"
+]
+NBINS_TO_SCAN=[1,2,3,4,5,6,7]
+
+#### Fake correction
+USE_FAKE_FIX = True   
+
+RUN_Z_NO_UNC=True
+
+def build_log_tag():
+    parts = []
+    
+    # Z treatment
+    if RUN_Z_NO_UNC:
+        parts.append("runZnoUnc")
+    else:
+        parts.append("runZwithUnc")
+        
+    # Fake treatment
+    if USE_FAKE_FIX:
+        parts.append("FakeFix")
+    else:
+        parts.append("NoFakeFix")
+        
+    # Nbins
+    if len(NBINS_TO_SCAN) > 1:
+        parts.append(f"NBin{min(NBINS_TO_SCAN)}to{max(NBINS_TO_SCAN)}")
+    else:
+        parts.append(f"NBin{NBINS_TO_SCAN[0]}")
+
+    return "_".join(parts)
+
+
+LOG_TAG = build_log_tag()
